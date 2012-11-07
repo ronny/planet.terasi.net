d60ffd0e01428f4dd91fc96f9f6b4ecd1b4569f8
Ronny Haryanto <ronny@haryan.to>
2012-11-07 18:30:21 +1100

    Ditched planet

--- attic/app.py
+++ attic/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, send_from_directory
 from gmail import Gmail
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
 
 @app.route('/')
 def index():
