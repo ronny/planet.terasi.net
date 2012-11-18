eb61659917bcb4b0874ab72ec9fa0b140b459100
Ronny Haryanto <ronny@haryan.to>
2012-11-18 23:35:51 +1100

    Try to get heroku to serve static files

--- aggregator/urls.py
+++ aggregator/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -15,3 +16,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('',
+    (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
+
