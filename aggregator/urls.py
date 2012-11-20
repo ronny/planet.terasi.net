751886fdf33256f654aa89d62982cf784df11ed2
Ronny Haryanto <ronny@haryan.to>
2012-11-20 17:15:21 +1100

    Offline assets

--- aggregator/urls.py
+++ aggregator/urls.py
@@ -17,10 +17,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-# urlpatterns += patterns('django.contrib.staticfiles.views',
-#     url(r'^static/(?P<path>.*)$', 'serve'),
-# )
-
 urlpatterns += patterns('',
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
